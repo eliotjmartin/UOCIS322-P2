a4105628de40aeb2a2ca56b91c94e5c76cbbe140
eliotjmartin <eliotm@uoregon.edu>
2021-04-19 15:40:17 -0700

    added better documentation

--- web/app.py
+++ web/app.py
@@ -6,19 +6,23 @@ from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    '''
+    """
     main page visited when run
-    '''
+    """
     return "UOCIS docker demo!\n"
+
 
 @app.route("/<name>")
 def redirect(name):
-    '''
-    handle situations where user is not just looking for 
-    the home page
-    '''
+    """
+    handle situations where user is not just looking for
+    the home page. Check for 403 and 404 errors and
+    redirect to appropriate html page in templates
+    folder.
+    """
     if '//' in name or '~' in name or '..' in name:
         abort(403)
     else:
@@ -27,13 +31,20 @@ def redirect(name):
         except:
             abort(404)
 
+
 @app.errorhandler(403)
 def forbidden(e):
+    """
+    Handle 403 error message
+    """
     return render_template("403.html")
 
 
 @app.errorhandler(404)
 def not_found(e):
+    """
+    Handle 404 error message
+    """
     return render_template("404.html")
 
 
