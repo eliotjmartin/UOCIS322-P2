2d8738b575de0da31b853148a62ce3e1871ca246
eliotjmartin <eliotm@uoregon.edu>
2021-04-20 14:50:24 -0700

    More comments on app.py

--- web/app.py
+++ web/app.py
@@ -15,7 +15,7 @@ def hello():
     return "UOCIS docker demo!\n"
 
 
-@app.route("/<path:name>")
+@app.route("/<path:name>")  # specify path to allow slashes (defaults to string which is problematic)
 def redirect(name):
     """
     handle situations where user is not just looking for
@@ -23,29 +23,31 @@ def redirect(name):
     redirect to appropriate html page in templates
     folder.
     """
-    if '//' in name or '~' in name or '..' in name:
+    if '//' in name or '~' in name or '..' in name:  # check for forbidden characters
         abort(403)
     else:
         try:
-            return render_template(name)
-        except:
+            return render_template(name)  # first try to open html file from templates
+        except:  # if render fails then the file isn't in templates folder -- abort
             abort(404)
 
 
 @app.errorhandler(403)
 def forbidden(e):
     """
-    Handle 403 error message
+    Handle 403 error message. Use render_template to look in templates folder
+    for html files
     """
-    return render_template("403.html"), 403
+    return render_template("403.html"), 403  # follow with 403 to specify STATUS_CODE
 
 
 @app.errorhandler(404)
 def not_found(e):
     """
-    Handle 404 error message
+    Handle 404 error message. Use render_template to look in templates folder
+    for html files
     """
-    return render_template("404.html"), 404
+    return render_template("404.html"), 404  # specify status code with 404
 
 
 if __name__ == "__main__":
